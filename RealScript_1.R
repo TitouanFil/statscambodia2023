ffa8f35eff2847ef38d01e86efbb26b92b20d66f
Titouan Filloux <titouan.filloux@wanadoo.fr>
2023-02-08 16:41:34 +0700

    2nd Session of statistical analysison VIRA phd results. Our aim is to determine effects of Treatment on variables + difference bewteen two years (2011 and 2021). We analyze 5 variables:
    - %N
    - %C
    - BD (Bulk Desnity)
    - C Stock (t/ha)
    - N Stock (t/ha)
    
    The data can be sliced based on 4 levels:
    - 1. Experiment (Maize, Soybean and Cassava)
    - 2. Year (2011, 2021)
    - 3. Soil depth (0-5, 5-10, 10-20, 20-40, 40-60, 60-80, 80-100)
    - 4. Treatment (CT,NT1,NT2,NT3)
    
    Wanted outputs (for each variables):
    - Clean data - OK
    - Descriptive statistics (mean & sd + boxplots) of treatments for each Expe/Year/depth combination - OK
    - Hypothesis check (test + normality) + ANOVA/Tukey to test significant differences between treatments for each Expe/Year/depth combination - OK
    - Diachronic analysis for each treatments between 2011 and 2021 for each Expe/depth combination - OK
    - Same than the two previous outputs for N and C stocks but in using cumulated stock values from several soil depths - OK
    - Linear plot showing the effect of soil depth on each variable (1 treatment = 1 curve), for each Expe/Year combination- OK
    
    The aim of this script is to create all required outputs by using different loop functions. It will be completed during a next work session.
    
    The 1st tep of this analysis is finished now and next step will be decided in the next meeting.

--- RealScript_1.R
+++ RealScript_1.R
@@ -15,6 +15,8 @@ TAB <- read.table("230205_Soil data arrangement for R 2011 vs 2021.csv", header 
 ##2. Data preparation
 #Reordering Depth factor values
 TAB$Depth <- factor(TAB$Depth, levels=c('0-5','5-10','10-20','20-40','40-60','60-80','80-100'))
+TAB$NSTOCK <- as.numeric(TAB$NSTOCK)
+TAB$NSTOCK2 <- as.numeric(TAB$NSTOCK2)
 #Checking for aberrant value through statistics
 summary(TAB)
 #-> We need to remove negative %N and C/N data
@@ -66,7 +68,7 @@ StCsumCasEx <- SUM[[14]][[3]]
 
 #Visualization - Boxplots
 Dept <- TAB6[!duplicated(TAB5$Depth),3]
-for (i in c(7,8,10,13,14)){
+for (i in c(7,8,10,13)){
   for (j in 1:3){
   TAB7 <- subset(TAB6, Experiment == Expe[j])
   for (k in 1:7){
@@ -77,9 +79,41 @@ for (i in c(7,8,10,13,14)){
     }
   }
 }
+#For the last variable, we need a 2nd loop as we create 105 plots and Rstudio 
+#can print only 100 at a time
+  for (j in 1:3){
+    TAB7 <- subset(TAB6, Experiment == Expe[j])
+    for (k in 1:7){
+      TAB8 <- subset(TAB7, Depth == Dept[k])
+      bxp <- ggboxplot(TAB8, x = "Year", y = colnames(TAB[14]),
+                       color = "Treatment", palette = "jco")+ ggtitle(paste(colnames(TAB[i]),Expe[j],Dept[k]))
+      print(bxp)
+    }
+  }
+#Variable value depending on soil depth plots
+#Table preparation with average values
+TAB6$ID <- paste(TAB6$Experiment,TAB6$Treatment,TAB6$Depth,TAB6$Year)
+TAB7 <- as.data.frame(apply(TAB6[,7:14],2,tapply, TAB6$ID, mean))
+ID <- TAB6[,c(1:3,5,16)]
+ID <- ID %>% distinct(ID, .keep_all = TRUE)
+TAB7$ID <- rownames(TAB7)
+TAB8 <- merge(ID, TAB7, by ="ID")
+for (i in c(7,8,10,13)){
+  for (j in 1:3){
+    TAB9 <- subset(TAB8, Experiment == Expe[j])
+    plot <- ggplot(TAB9, aes(x = Depth, y = TAB9[,i], group = Treatment,color = Treatment)) +
+    geom_line(aes(color=Treatment))+ geom_point(aes(color=Treatment)) + facet_grid(cols =vars(Year))+
+    ggtitle(colnames(TAB9[i]),Expe[j])
+    print(plot)
+    }
+  }
 
-##4. hypothesis checking + ANOVA
+
+##4. hypothesis checking + ANOVA for each soil depth individually
 #Normality hypothesis
+#For this part, all plots and text will be created but only the last part will appear
+#As R console and plot had limit. Better to run variable one by one if needed
+#to get all results
 Yea <- TAB6[!duplicated(TAB5$Yea),5]
 for (i in c(7,8,10,13,14)){
   for (j in 1:3){
@@ -100,7 +134,8 @@ for (i in c(7,8,10,13,14)){
         print(paste(colnames(TAB9[i]),Expe[j],Yea[k],Dept[l]))
         print(shapiro_test(residuals(model)))
         #Homoscedasticity plot
-        
+        plot2 <- plot(model, 1, main = paste("Normality",colnames(TAB9[i]),Expe[j],Yea[k],Dept[l]))
+        print(plot2)
         #Homoscedasticity test
         print(TAB9 %>% levene_test(TAB9[,i] ~ Treatment))
         #ANOVA + pairtests
@@ -112,7 +147,10 @@ for (i in c(7,8,10,13,14)){
 }
 
 
-### Modeling
+###5. Modeling for each soil depth individually
+#For this part, all plots and text will be created but only the last part will appear
+#As R console and plot had limit. Better to run variable one by one if needed
+#to get all results
 for (i in c(7,8,10,13,14)){
   for (j in 1:3){
     TAB7 <- subset(TAB6, Experiment == Expe[j])
@@ -141,10 +179,69 @@ for (i in c(7,8,10,13,14)){
   }
 }
 
+###6. Creation of cumulated stock for soil depths
+TAB6$ID <- paste(TAB6$Experiment,TAB6$Treatment,TAB6$Replicate,TAB6$Year)
+ID <- TAB6[,c(1:2,4:5,16)]
+ID <- ID %>% distinct(ID, .keep_all = TRUE)
+# Data prep For Cumulated layer 0-10, 0-20, 0-60, 0-100
+#0-10
+TAB010 <- subset(TAB6, Depth == "0-5" | Depth == "5-10")
+TAB010Cum <- as.data.frame(apply(TAB010[,13:14],2,tapply, TAB010$ID, sum))
+TAB010Cum$ID <- rownames(TAB010Cum)
+TABCumDept <- merge(ID, TAB010Cum, by ="ID")
+colnames(TABCumDept)[6] <- paste("NSTOCK2-010")
+colnames(TABCumDept)[7] <- paste("CSTOCK2-010")
+#0-20
+TAB020 <- subset(TAB6, Depth == "0-5" | Depth == "5-10" | Depth == "10-20")
+TAB020Cum <- as.data.frame(apply(TAB020[,13:14],2,tapply, TAB020$ID, sum))
+TAB020Cum$ID <- rownames(TAB020Cum)
+TABCumDept2 <- merge(TABCumDept, TAB020Cum, by ="ID")
+colnames(TABCumDept2)[8] <- paste("NSTOCK2-020")
+colnames(TABCumDept2)[9] <- paste("CSTOCK2-020")
+#0-60
+TAB060 <- subset(TAB6, Depth != "60-80" | Depth == "80-100")
+TAB060Cum <- as.data.frame(apply(TAB060[,13:14],2,tapply, TAB060$ID, sum))
+TAB060Cum$ID <- rownames(TAB060Cum)
+TABCumDept3 <- merge(TABCumDept2, TAB060Cum, by ="ID")
+colnames(TABCumDept3)[10] <- paste("NSTOCK2-060")
+colnames(TABCumDept3)[11] <- paste("CSTOCK2-060")
+#0-100
+TAB0100 <- TAB6
+TAB0100Cum <- as.data.frame(apply(TAB0100[,13:14],2,tapply, TAB0100$ID, sum))
+TAB0100Cum$ID <- rownames(TAB0100Cum)
+TABCumDept4 <- merge(TABCumDept3, TAB0100Cum, by ="ID")
+colnames(TABCumDept4)[12] <- paste("NSTOCK2-0100")
+colnames(TABCumDept4)[13] <- paste("CSTOCK2-0100")
 
 
-Yea[k]
-i = 8
-j = 1
-k = 1
-l = 1
+###7. Modeling for cumulated soil depths
+#For this part, all plots and text will be created but only the last part will appear
+#As R console and plot had limit. Better to run variable one by one if needed
+#to get all results
+for (i in c(6:13)){
+  for (j in 1:3){
+    TABCumDept5 <- subset(TABCumDept4, Experiment == Expe[j])
+      print("")
+      print("")
+      print(paste(colnames(TABCumDept4[i]),"Cumulated",Expe[j]))
+      #Model creation
+      mod=lmer(TABCumDept4[,i] ~ Treatment*Year+(1|Replicate),data=TABCumDept4)
+      #ANOVA, fisher test results
+      anova(mod)
+      #emmeans + Tukey
+      moyTreatByYea =emmeans(mod, ~Treatment|Year)
+      tukTreatByYea=cld(moyTreatByYea)
+      print(tukTreatByYea)
+      #Plot
+      plot <- ggplot(tukTreatByYea, aes_string(x="Year", y="emmean", colour="Treatment", group="Treatment")) +
+        geom_line(aes_string(linetype="Treatment"), size=.6) +
+        geom_point(aes_string(shape="Treatment"), size=3) +
+        geom_errorbar(aes_string(ymax="upper.CL", ymin="lower.CL"), width=.1) +
+        ggtitle(paste(colnames(TABCumDept4[i]),"Cumulated",Expe[j]))+
+        scale_x_continuous(breaks = c(2011,2021))
+      print(plot)
+    }
+  }
+
+
+
